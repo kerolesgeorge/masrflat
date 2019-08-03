61251d33947cc0b2c0a1267a9bea93434d1960c3
Keroles <kerolesgeorge@hotmail.com>
2019-08-03 02:37:08 +0200

    Creating homepage hero image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,90 +7,31 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    {{ __('text.logo') }}
-                </div>
-                <p class="h1">ابحث عن عقارك في مصر الجديده</p class="title h2">
-                <div class="links">
-                    <a href="#">من نحن</a>
-                    <a href="#">للأتصال بنا</a>
-                    <a href="#">عقارات</a>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="#">شقة في مصر الجديده</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse mr-3" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="#">من نحن</a>
+                    <a class="nav-item nav-link" href="#">اتصل بنا</a>
+                    <a class="nav-item nav-link" href="#">عقارات</a>
                 </div>
             </div>
-        </div>
+        </nav>
+        <main>
+            <div class="hero-image">
+                <div class="hero-text">
+                    <h1>شقة في مصر الجديده</h1>
+                    <p>ابحث عن عقار للبيع في مصر الجديده</p>
+                </div>
+            </div>
+        </main>
     </body>
 </html>
